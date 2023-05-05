cadc7dc0fb0ec314b47d908a940a082f2414d251
Andy Zhang <54629263+andyy0624@users.noreply.github.com>
2023-05-06 07:13:13 +0800

    修正API: 配合BahaCrawler的翻修，修正method的使用方式及修改變數名稱

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from typing import Optional, Union
 from fastapi import FastAPI
 import random as rd
-from baha_utils import BahaCrawler
-from baha_utils import ArticleHandler
 
+from baha_utils import BahaCrawler, BahaWebArguments, QuerryParams, WAIT_TIME, INF
 
 app = FastAPI()  # 建立一個 Fast API application
 page_engine = "selenium"  # VERY SLOW, NOT RECOMMENDED
@@ -27,24 +26,27 @@ def get_pages_article_urls(
 
 @app.get("/baha/get_article_contents")
 def get_article_contents(
-    bsn: Union[int, str],
-    snA: Union[int, str],
-    limit_page: Optional[int] = float("inf"),
-    wait_time: Optional[Union[int, float]] = (rd.random() * 4) + 1,
+    url: Optional[str],
+    bsn: Optional[int],
+    snA: Optional[int],
+    sub_pages_limit_num: Optional[int] = INF,
+    sub_pages_wait_time: Optional[Union[int, float]] = WAIT_TIME,
 ) -> dict:
-    return ArticleHandler(
-        bsn=bsn, snA=snA, page_engine=page_engine
-    ).get_article_contents(limit_page, wait_time)
+    return crawler.get_article_contents(
+        BahaWebArguments(url, QuerryParams(bsn, snA)),
+        sub_pages_limit_num,
+        sub_pages_wait_time,
+    )
 
 
 @app.get("/baha/get_pages_article_contents")
 def get_pages_article_contents(
     bsn: Union[int, str],
-    limit_subPage: Optional[int] = 1,
+    sub_pages_limit_num: Optional[int] = 1,
     start_page: Optional[Union[int, str]] = 1,
     end_page: Optional[Union[int, str]] = None,
     wait_time: Optional[Union[int, float]] = (rd.random() * 4) + 1,
 ) -> list:
     return crawler.get_pages_article_contents(
-        bsn, limit_subPage, start_page, end_page, wait_time
+        bsn, sub_pages_limit_num, start_page, end_page, wait_time
     )
