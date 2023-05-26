2c523e094928dc6db72b7b9be1fb9068d1d448f4
Andy Zhang <54629263+andyy0624@users.noreply.github.com>
2023-05-26 10:10:34 +0800

    新增巴哈搜索頁面的爬取功能

--- baha_utils/baha_crawler.py
+++ baha_utils/baha_crawler.py
@@ -9,17 +9,19 @@ from .article_handler import (
     WebArguments,
     BAHA_URL,
     B_PAGE,
+    SEARCH_PAGE,
     WAIT_TIME_FN,
     INF,
 )
+from .datastructures import QueryParams
 
 
 class BahaCrawler:
     def __init__(self, page_engine: Optional[str] = "requests") -> None:
         self._page_engine = page_engine
 
-    def get_page_article_urls(self, web_args_b: WebArguments) -> list:
-        url_builder = UrlBuilder(f"{BAHA_URL}/{B_PAGE}")
+    def get_forum_article_url(self, web_args_b: WebArguments) -> list:
+        url_builder = UrlBuilder(QueryParams.BPage, f"{BAHA_URL}/{B_PAGE}")
         url = url_builder(**asdict(web_args_b.query_params))
         soup = WebRequester("requests")(url)
 
@@ -34,18 +36,41 @@ class BahaCrawler:
             article_urls += [article_url]
         return article_urls
 
+    def get_searched_article_url(
+        self,
+        web_args_search: WebArguments,
+    ) -> list:
+        url_builder = UrlBuilder(QueryParams.SearchPage, f"{BAHA_URL}/{SEARCH_PAGE}")
+        url = url_builder(**asdict(web_args_search.query_params))
+        soup = WebRequester("requests")(url)
+
+        article_urls = []
+        item_blocks = soup.select("div[class^='search-result_article'] h2[class='search-result_title']")
+        if len(item_blocks) == 0:
+            raise ValueError({"Error": "網頁沒有任何文章列表，你可能指向了錯誤的頁面", "URL": url})
+
+        for item_block in item_blocks:
+            title_block = item_block.select_one("h2[class='search-result_title'] > a")
+            article_url = f"{title_block.get('href')}"
+            article_urls += [article_url]
+        return article_urls
+    
+
     def get_pages_article_urls(
         self,
         web_args_b: WebArguments,
+        page_type: Optional[int] = "forum",
         start_page: Optional[int] = 1,
         end_page: Optional[int] = None,
     ) -> list:
+        method_mapping = {"forum": self.get_forum_article_url,
+                          "searched": self.get_searched_article_url}
         if end_page is None:
             end_page = start_page
         article_urls = []
         for page in range(start_page, end_page + 1):
             web_args_b.query_params.page = page
-            article_urls += self.get_page_article_urls(web_args_b)
+            article_urls += method_mapping[page_type](web_args_b)
         return article_urls
 
     def get_article_content(
@@ -65,6 +90,7 @@ class BahaCrawler:
     def get_pages_article_contents(
         self,
         web_args_b: WebArguments,
+        page_type: Optional[int] = "forum",
         text_only: Optional[int] = 1,
         start_page: Optional[int] = 1,
         end_page: Optional[int] = None,
@@ -72,11 +98,11 @@ class BahaCrawler:
         sub_page_wait_time_fn: Optional[Callable] = WAIT_TIME_FN,
     ) -> list:
         web_args_b.page_engine = self._page_engine
-        article_urls = self.get_pages_article_urls(web_args_b, start_page, end_page)
+        article_urls = self.get_pages_article_urls(web_args_b, page_type , start_page, end_page)
 
         article_contents = []
         for article_url in article_urls:
-            web_args_c = WebArguments(url=article_url,page_engine=self._page_engine)
+            web_args_c = WebArguments(url=article_url, page_engine=self._page_engine)
             article_contents += [
                 ArticleHandler(web_args_c).get_article_content(
                     text_only=text_only,
