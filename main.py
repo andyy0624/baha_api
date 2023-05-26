dd414dfc0634559d52985bfa155fa48ebc9c4841
Andy Zhang <54629263+andyy0624@users.noreply.github.com>
2023-05-26 10:07:09 +0800

    新增巴哈搜索頁面功能

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from urllib.parse import parse_qs
 from baha_utils import BahaCrawler, WebArguments, QueryParams, WAIT_TIME_FN, INF, convert_qs_to_dict
 
 app = FastAPI()  # 建立一個 Fast API application
-page_engine = "selenium"  # VERY SLOW, NOT RECOMMENDED
-# page_engine = "requests"
+# page_engine = "selenium"  # VERY SLOW, NOT RECOMMENDED
+page_engine = "requests"
 crawler = BahaCrawler(page_engine)
 
 
@@ -19,7 +19,7 @@ def read_root():
 # baha_urls
 @app.get("/baha/urls/default/{baha_query_string}")
 def baha_urls_default(baha_query_string: str) -> list:
-    query_params_dict = convert_qs_to_dict(baha_query_string)
+    query_params_dict = convert_qs_to_dict(QueryParams.BPage, baha_query_string)
     return crawler.get_pages_article_urls(
         web_args_b=WebArguments(query_params=QueryParams.BPage(**query_params_dict))
     )
@@ -27,18 +27,36 @@ def baha_urls_default(baha_query_string: str) -> list:
 
 @app.get("/baha/urls/{crawler_arg_string}/{baha_query_string}")
 def baha_urls(crawler_arg_string: str, baha_query_string: str) -> list:
-    crawler_arg_dict = convert_qs_to_dict(crawler_arg_string)
-    query_params_dict = convert_qs_to_dict(baha_query_string)
+    crawler_arg_dict = convert_qs_to_dict(object, crawler_arg_string)
+    query_params_dict = convert_qs_to_dict(object, baha_query_string)
+    if("page_type" not in crawler_arg_dict):
+        crawler_arg_dict = convert_qs_to_dict(QueryParams.BPage, crawler_arg_string)
+        query_params_dict = convert_qs_to_dict(QueryParams.BPage, baha_query_string)
+        return crawler.get_pages_article_urls(
+            web_args_b=WebArguments(query_params=QueryParams.BPage(**query_params_dict)),
+            **crawler_arg_dict,
+        )
+    
+    if(crawler_arg_dict["page_type"]=="searched"):
+        crawler_arg_dict = convert_qs_to_dict(QueryParams.SearchPage, crawler_arg_string)
+        query_params_dict = convert_qs_to_dict(QueryParams.SearchPage, baha_query_string)
+        return crawler.get_pages_article_urls(
+            web_args_b=WebArguments(query_params=QueryParams.SearchPage(**query_params_dict)),
+            **crawler_arg_dict,
+        )
+        
+    crawler_arg_dict = convert_qs_to_dict(QueryParams.BPage, crawler_arg_string)
+    query_params_dict = convert_qs_to_dict(QueryParams.BPage, baha_query_string)
     return crawler.get_pages_article_urls(
-        web_args_b=WebArguments(query_params=QueryParams.BPage(**query_params_dict)),
-        **crawler_arg_dict,
-    )
+            web_args_b=WebArguments(query_params=QueryParams.BPage(**query_params_dict)),
+            **crawler_arg_dict,
+        )
 
 
 # baha_article
 @app.get("/baha/article/default/{baha_query_string}")
 def baha_article_default(baha_query_string: str) -> dict:
-    query_params_dict = convert_qs_to_dict(baha_query_string)
+    query_params_dict = convert_qs_to_dict(QueryParams.CPage, baha_query_string)
     return crawler.get_article_content(
         web_args_c=WebArguments(query_params=QueryParams.CPage(**query_params_dict))
     )
@@ -46,8 +64,8 @@ def baha_article_default(baha_query_string: str) -> dict:
 
 @app.get("/baha/article/{crawler_arg_string}/{baha_query_string}")
 def baha_article(crawler_arg_string: str, baha_query_string: str) -> dict:
-    query_params_dict = convert_qs_to_dict(baha_query_string)
-    crawler_arg_dict = convert_qs_to_dict(crawler_arg_string)
+    query_params_dict = convert_qs_to_dict(QueryParams.CPage, baha_query_string)
+    crawler_arg_dict = convert_qs_to_dict(QueryParams.CPage, crawler_arg_string)
     return crawler.get_article_content(
         web_args_c=WebArguments(query_params=QueryParams.CPage(**query_params_dict)),
         **crawler_arg_dict,
@@ -56,7 +74,7 @@ def baha_article(crawler_arg_string: str, baha_query_string: str) -> dict:
 # baha_articles
 @app.get("/baha/articles/default/{baha_query_string}")
 def baha_articles(baha_query_string: str) -> list:
-    query_params_dict = convert_qs_to_dict(baha_query_string)
+    query_params_dict = convert_qs_to_dict(QueryParams.BPage, baha_query_string)
     return crawler.get_pages_article_contents(
         web_args_b=WebArguments(query_params=QueryParams.BPage(**query_params_dict)),
     )
@@ -64,8 +82,28 @@ def baha_articles(baha_query_string: str) -> list:
 
 @app.get("/baha/articles/{crawler_arg_string}/{baha_query_string}")
 def baha_articles(crawler_arg_string: str, baha_query_string: str) -> list:
-    query_params_dict = convert_qs_to_dict(baha_query_string)
-    crawler_arg_dict = convert_qs_to_dict(crawler_arg_string)
+    query_params_dict = convert_qs_to_dict(object, baha_query_string)
+    crawler_arg_dict = convert_qs_to_dict(object, crawler_arg_string)
+    if("page_type" not in crawler_arg_dict):
+        crawler_arg_dict = convert_qs_to_dict(QueryParams.BPage, crawler_arg_string)
+        query_params_dict = convert_qs_to_dict(QueryParams.BPage, baha_query_string)
+        return crawler.get_pages_article_contents(
+            web_args_b=WebArguments(query_params=QueryParams.BPage(**query_params_dict)),
+            **crawler_arg_dict,
+        )
+    
+    if(crawler_arg_dict["page_type"]=="searched"):
+        crawler_arg_dict = convert_qs_to_dict(QueryParams.SearchPage, crawler_arg_string)
+        query_params_dict = convert_qs_to_dict(QueryParams.SearchPage, baha_query_string)
+        return crawler.get_pages_article_contents(
+            web_args_b=WebArguments(query_params=QueryParams.SearchPage(**query_params_dict)),
+            **crawler_arg_dict,
+        )
+    
+    
+    
+    query_params_dict = convert_qs_to_dict(QueryParams.BPage, baha_query_string)
+    crawler_arg_dict = convert_qs_to_dict(QueryParams.BPage, crawler_arg_string)
     return crawler.get_pages_article_contents(
         web_args_b=WebArguments(query_params=QueryParams.BPage(**query_params_dict)),
         **crawler_arg_dict,
